1857852e3b009b4c51f62e6a11c274aeed6ad991
Akash <126044118+AKA2114SH@users.noreply.github.com>
2025-02-06 22:39:18 +0530

    Multiple inheritance-interface

---
+++ task_4_feb_2025/Multiple_Inheritance_Interface/Swimmable.java
@@ -0,0 +1,5 @@
+package task_4_feb_2025.Multiple_Inheritance_Interface;
+
+public interface Swimmable {
+
+}
---
+++ task_4_feb_2025/Multiple_Inheritance_Interface/Test.java
@@ -0,0 +1,10 @@
+package task_4_feb_2025.Multiple_Inheritance_Interface;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

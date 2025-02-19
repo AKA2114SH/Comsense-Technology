fab869bcf8a96dd6927e4b190e4521cf56758ec2
Akash <126044118+AKA2114SH@users.noreply.github.com>
2025-02-19 16:04:04 +0530

    Constructor Example

---
+++ Constructors/Example/demo.java
@@ -0,0 +1,9 @@
+package Constructors.Example;
+
+public class demo {
+    public static void main(String[] args) {
+        test obj = new test();
+        obj.display();
+    }
+
+}
---
+++ Constructors/Example/test.java
@@ -0,0 +1,17 @@
+package Constructors.Example;
+
+public class test {
+    int a;
+    String name;
+
+    test() {
+        a = 10;
+        name = "Akash";
+    }
+
+    void display() {
+        System.out.println(a);
+        System.out.println(name);
+    }
+
+}
